45ca1fcc62a1eed8e7f2034e52d26a77e7572083
matheus-wisniewski <maahwiski@gmail.com>
2023-06-06 09:21:32 -0300

    req 01

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
+const dataPath = './talker.json';
+
+const readData = async () => {
+  try {
+    const read = await fs.readFile(path.resolve(__dirname, dataPath), 'utf-8');
+    return JSON.parse(read);
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -13,4 +25,13 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+app.get('/talker', async (req, res) => {
+  const talkerData = await readData();
+  if (talkerData) {
+    res.status(HTTP_OK_STATUS).json(talkerData);
+  } else {
+    return [];
+  }
 });
